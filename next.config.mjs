fdb064530fbaae2a540b22c9ca8d89eda6e2eb17
gabotechs <gb.mt.me@gmail.com>
2024-06-29 17:34:26 +0200

    Remove useless config

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    rewrites: async () => {
-        const rewrites = {
-            afterFiles: [
-                // apply any of your existing rewrites here
-            ],
-            fallback: []
-        }
-
-        // dev only, this allows for local api calls to be proxied to
-        // api routes that use rust runtime
-        if (process.env.NODE_ENV === 'development') {
-            rewrites.fallback.push({
-                source: '/api/:path*',
-                destination: 'http://0.0.0.0:3001/api/:path*'
-            })
-        }
-
-        return rewrites
-    },
-    // https://github.com/orgs/vercel/discussions/103#discussioncomment-6356642
-    experimental: {
-        outputFileTracingExcludes: {
-            '*': [
-                'node_modules/@swc/core-linux-x64-gnu',
-                'node_modules/@swc/core-linux-x64-musl',
-                'node_modules/@esbuild/linux-x64',
-            ],
-        },
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
