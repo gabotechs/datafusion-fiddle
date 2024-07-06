9526622b26d2b9179931e0b2cadae2dd1341ba31
gabotechs <gb.mt.me@gmail.com>
2024-07-06 10:16:22 +0200

    Fix nextjs compile

--- app/useScreenWidth.ts
+++ app/useScreenWidth.ts
@@ -1,19 +1,18 @@
 import { useState, useEffect } from 'react';
 
 export function useScreenWidth() {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const innerWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+  const [screenWidth, setScreenWidth] = useState(innerWidth);
 
   useEffect(() => {
-    const handleResize = () => {
+    function handleResize () {
       setScreenWidth(window.innerWidth);
-    };
+    }
 
     window.addEventListener('resize', handleResize);
 
     // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return screenWidth;
