4ed96299caba39856972eba5196535e337c57644
Rob Tandy <rob.tandy@datadoghq.com>
2025-09-12 11:26:08 -0400

    add graphviz output

--- src/useApi.ts
+++ src/useApi.ts
@@ -1,65 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import * as Viz from "@viz-js/viz";
 
 export interface SqlRequest {
-  stmts: string[]
-  distributed: boolean
+  stmts: string[];
+  distributed: boolean;
 }
 
 export interface SqlResponse {
-  columns: Array<[string, string]>,
-  rows: Array<Array<string>>,
-  logical_plan: string
-  physical_plan: string
+  columns: Array<[string, string]>;
+  rows: Array<Array<string>>;
+  logical_plan: string;
+  physical_plan: string;
+  graphviz_plan: string;
 }
 
-export async function executeStatements (stmts: string[], distributed: boolean): Promise<SqlResponse> {
+export async function executeStatements(
+  stmts: string[],
+  distributed: boolean,
+): Promise<SqlResponse> {
   const req: SqlRequest = {
     stmts,
-    distributed
-  }
-  const res = await fetch(
-    '/api/main',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(req),
-    }
-  )
+    distributed,
+  };
+  const res = await fetch("/api/main", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req),
+  });
   if (res.status === 200) {
-    return await res.json()
+    return await res.json();
   } else if (res.status === 400) {
-    const { message } = await res.json()
-    throw new Error(message)
+    const { message } = await res.json();
+    throw new Error(message);
   } else {
-    const msg = await res.text()
-    throw new Error(`unexpected status ${res.status}: ${msg}`)
+    const msg = await res.text();
+    throw new Error(`unexpected status ${res.status}: ${msg}`);
   }
 }
 
 export type ApiState =
-  { type: 'nothing' } |
-  { type: 'loading' } |
-  { type: 'error', message: string } |
-  { type: 'result', result: SqlResponse }
+  | { type: "nothing" }
+  | { type: "loading" }
+  | { type: "error"; message: string }
+  | { type: "result"; result: SqlResponse };
 
 export interface ApiRequest {
-  statement: string
-  distributed: boolean
+  statement: string;
+  distributed: boolean;
 }
 
-export function useApi () {
-  const [state, setState] = useState<ApiState>({ type: 'nothing' });
+export function useApi() {
+  const [state, setState] = useState<ApiState>({ type: "nothing" });
 
   const execute = React.useCallback(async (req: ApiRequest) => {
-    setState({ type: 'loading' });
-    const result = await executeStatements(
-      req.statement.split(';').map(_ => _.trim()).filter(_ => _.length > 0),
-      req.distributed
-    )
-      .then((result) => ({ type: 'result' as const, result }))
-      .catch((err) => ({ type: 'error' as const, message: err.toString() }));
-    setState(result)
-    return result
+    setState({ type: "loading" });
+    try {
+      const result = await executeStatements(
+        req.statement
+          .split(";")
+          .map((_) => _.trim())
+          .filter((_) => _.length > 0),
+        req.distributed,
+      );
+
+      if (result.graphviz_plan.length > 0) {
+        const viz = await Viz.instance();
+        try {
+          const graphviz_plan = viz.renderSVGElement(result.graphviz_plan);
+          console.log("graphviz_plan", graphviz_plan);
+          result.graphviz_plan = graphviz_plan.outerHTML;
+        } catch (e) {
+          console.error("failed to render graphviz plan", e);
+        }
+      }
+      setState({ type: "result", result });
+      return {
+        type: "result" as const,
+        result,
+      };
+    } catch (error) {
+      const err = error as Error;
+      return { type: "error" as const, message: err.toString() };
+    }
   }, []);
 
   return { state, execute };
